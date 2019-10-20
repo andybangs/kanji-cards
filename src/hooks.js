0044f298ce6a546af13dda7aa57cefcf9f1d9eb9
Andy Bangs <andrew.yuta@gmail.com>
2019-10-20 11:21:11 -0700

    Update deps

--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { useSpring } from 'react-spring';
-import { useGesture } from 'react-use-gesture';
+import { useDrag } from 'react-use-gesture';
 
 export function useFlip(state) {
   const [flipped, setFlip] = useState(state);
@@ -15,7 +15,7 @@ export function useFlip(state) {
 
 export function useSwipe({ onSwipeLeft, onSwipeRight }) {
   const [{ x, y }, set] = useSpring(() => ({ x: 0, y: 0 }));
-  const bindSwipe = useGesture(({ down, delta: [xDelta], direction: [xDir, yDir], velocity }) => {
+  const bindSwipe = useDrag(({ down, movement: [xDelta], direction: [xDir, yDir], velocity }) => {
     const trigger = velocity > 0.2;
     const dir = xDir < 0 ? -1 : 1;
     const isSideSwipe = yDir > -0.5 && yDir < 0.5;
